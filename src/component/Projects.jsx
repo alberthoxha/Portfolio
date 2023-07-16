a4b3c2c7d7fcd2f414e68d79175ed02fcaa576fc
Albert Hoxha <123065766+alberthoxha@users.noreply.github.com>
2023-07-16 10:55:12 +0200

    adding style around web (#3)

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -1,4 +1,5 @@
 import "./Projects.css"
+import upcorn from "../assets/upcorn.jpg"
 
 export default function Projects() {
   return (
@@ -7,27 +8,45 @@ export default function Projects() {
         <h1>Projects</h1>
         <div className="contents">
           <div className="product-grid">
-            <div className="card stacked featured">
+            <a
+              href="https://usepopcorn-ah.vercel.app/"
+              target="_blank"
+              className="card stacked featured"
+              rel="noreferrer"
+            >
               <div className="card__content">
                 <h2 className="title">Usepopcorn</h2>
                 <p className="card__desc">
-                  This movie application i made during the course from Jonas
+                  This application is designed to provide a user-friendly
+                  interface for browsing and exploring movies.
                 </p>
               </div>
+              <img src={upcorn} alt="upcornimg" />
               <h3 className="card__lang">React JS</h3>
-            </div>
+            </a>
 
-            <a href="https://expediz.netlify.app/" target="_blank">
+            <a
+              href="https://expediz.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
               <div className="card stacked">
                 <div className="card__content">
                   <h2 className="title">Expediz</h2>
-                  <p className="card__desc">Your Travel Note-taking Web App.</p>
+                  <p className="card__desc">
+                    Expediz is a versatile web application designed to enhance
+                    your travel.
+                  </p>
                 </div>
                 <h3 className="card__lang">React JS</h3>
               </div>
             </a>
 
-            <a href="https://huddle-web-app.netlify.app" target="_blank" >
+            <a
+              href="https://huddle-web-app.netlify.app"
+              target="_blank"
+              rel="noreferrer"
+            >
               <div className="card stacked">
                 <div className="card__content">
                   <h2 className="title">Huddle</h2>
@@ -39,7 +58,11 @@ export default function Projects() {
               </div>
             </a>
 
-            <a href="https://magic-memory-q8ie5xmr4-alberthoxha.vercel.app/" target="_blank">
+            <a
+              href="https://magic-memory-q8ie5xmr4-alberthoxha.vercel.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
               <div className="card stacked">
                 <div className="card__content">
                   <h2 className="title">Memory Game</h2>
@@ -52,12 +75,17 @@ export default function Projects() {
               </div>
             </a>
 
-            <a href="https://magic-memory-q8ie5xmr4-alberthoxha.vercel.app/" target="_blank">
+            <a
+              href="https://supa-smoothie.vercel.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
               <div className="card stacked">
                 <div className="card__content">
                   <h2 className="title">Supa Smothie</h2>
                   <p className="card__desc">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    This repository contains a CRUD app built using ReactJS and
+                    integrated with Supabase
                   </p>
                 </div>
                 <h3 className="card__lang">React JS & Supabase</h3>
